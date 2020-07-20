009cec076a96ff6c3819d5abe533d4d14d63d764
Shagoy <pat.wolleb@gmail.com>
2020-07-20 14:17:55 +0200

    Do not need goal

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,4 @@ echo "::set-output name=time::$time"
 
 #!/usr/bin/env bash
 
-chmod 700 /opt/algorand/node/data/kmd-v0.5
-
 /opt/algorand/node/goal kmd start -d /opt/algorand/node/data
-/opt/algorand/node/algod -d /opt/algorand/node/data
