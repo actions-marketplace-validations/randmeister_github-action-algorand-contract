c654e62755527b82c199f6786ad60e2fc4fd34e2
Shagoy <pat.wolleb@gmail.com>
2020-07-20 14:08:07 +0200

    Test remote build

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,10 @@
 echo "API KEY $1"
 time=$(date)
 echo "::set-output name=time::$time"
+
+#!/usr/bin/env bash
+
+chmod 700 /opt/algorand/node/data/kmd-v0.5
+
+/opt/algorand/node/goal kmd start -d /opt/algorand/node/data
+/opt/algorand/node/algod -d /opt/algorand/node/data
